6968bda547ff92b003d588998efb37a97fe5ea28
sunilkumarm048 <55477491+sunilkumarm048@users.noreply.github.com>
2026-06-30 21:25:46 +0530

    Update page.tsx

--- src/app/customer/book/[providerId]/page.tsx
+++ src/app/customer/book/[providerId]/page.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useMemo, useState } from 'react';
 import { useRouter, useParams } from 'next/navigation';
-import Link from 'next/link';
+import Link from 'next/link' ;
 import {
   Loader2,
   ArrowLeft,
